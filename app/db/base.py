62a7b3606050ec3d1e4b98a1fc2da01656e2f8c0
ant-laz <alazzaro@google.com>
2025-05-20 21:49:00 +0000

    refactors api to have minimal code per endpoint & all db logic in a central library

--- app/db/base.py
+++ app/db/base.py
@@ -18,8 +18,10 @@
 ###############################################################################
 
 import os
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData, select
 from sqlalchemy.engine import Engine
+from sqlalchemy.schema import Table, Column
+from sqlalchemy.orm import Session
 
 # fetch environment variables to tell SQLAlchemy where to find our DB
 instanceid = os.environ["SPANNER_INSTANCE_ID"]
@@ -34,6 +36,52 @@ engine: Engine = create_engine(
     echo=True
 )
 
+# Database metadata refers to objects that represnt Tables & Columns
+# MetaData is a collection (e.g. py dict) that stores Tables
+# table = Table("Singers", MetaData(bind=engine), autoload=True)
+metadata_obj: MetaData = MetaData()
+
+# Once we have a MetaData object, we can declare some Table objects
+# We use a feature of SQLAlchemy called table reflection to reduce code required
+# Table reflection refers to the process of generating Table 
+# and related objects by reading the current state of a database. 
+# This way we avoid hardcoding Table schema which can go out of date !
+# The following code fetches metadata about all tables in our databse
+# ref : 
+# https://docs.sqlalchemy.org/en/20/core/reflection.html#reflecting-all-tables-at-once
+metadata_obj.reflect(bind=engine)
+
 # Dependency to get the database engine
 def get_db_engine() -> Engine:
     return engine
+
+def get_db_metadata() -> MetaData: 
+    return metadata_obj
+
+def get_info_tbl_with_1_col_pk(table_name: str, metadata: MetaData):
+    my_table: Table = metadata_obj.tables[table_name]
+    my_table_pk_column: Column = my_table.primary_key.columns[0]
+    my_table_pk_column_name: str = my_table_pk_column.name 
+    return (my_table, my_table_pk_column, my_table_pk_column_name)
+
+def get_page_of_data(engine: Engine, tbl: Table, pk_col: Column, pk_col_name: str, cursor: int, page_size: int):
+    # create a container to store the information fetched from the db
+    # # set default value of "next_cursor" to current cursor to handle
+    # # edge case when we have reached the end of the data in the db
+    data = {"records":[], "next_cursor": cursor}
+    
+    # Using a session, first create a select statement
+    # # implement cursor based pagination used the provided cursor & page_size provided in the api call
+    # # parse the result set into a json object which can be returned
+    with Session(engine) as session:
+        stmt = (select(tbl)
+                .where(pk_col >= cursor)
+                .order_by(pk_col.asc())
+                .limit(page_size))
+        result = session.execute(stmt)
+        for row in result:
+            this_row: dict = row._asdict()
+            data["records"].append(this_row)
+            data["next_cursor"] = int(this_row[pk_col_name])+1
+    
+    return data
--- app/main.py
+++ app/main.py
@@ -19,206 +19,47 @@
 # because of this, main.py is a "Python module" of "Python package" app. It is reffered to via app.main
 ###############################################################################
 
-import os
-
-from fastapi import FastAPI, Response
+from fastapi import FastAPI
 from fastapi.params import Depends
 
-from google.cloud import spanner
-
-from sqlalchemy import MetaData, Table, create_engine, select
-from sqlalchemy.orm import Session
-
-from app.db.base import get_db_engine
+from app.db.base import get_db_engine, get_db_metadata, get_info_tbl_with_1_col_pk, get_page_of_data
 
 # Create an instance of the FastAPI class
 musicapi = FastAPI()
 
 ###############################################################################
-# 0. API Health Check
+# API Health Check
 # ---------------------
-# create a path (aka endpoint or route) + operation
-# path is the base path "/"
-# operation is one of the HTTP methods, in this case GET
-# the "@" is a python decorator that tells Fast API the "root" method is for GET operations on "/"
-# this endpoint is a "health check" to allow developers to assess if deployemnts have been successful
 ###############################################################################
 @musicapi.get("/")
 def healthcheck():
-    return Response("server is running")
+    return Response("server is running")    
 
 ###############################################################################
-# Testing dependency injection
-###############################################################################
-@musicapi.get("/di")
-def di_test(cursor: int, page_size: int, engine = Depends(get_db_engine)):
-
-    metadata_obj = MetaData()
-
-    singers_table = Table("Singers", metadata_obj, autoload_with=engine)
-    
-    data = {"records":[], "next_cursor": cursor}
-
-    with Session(engine) as session:
-        stmt = select(singers_table).where(singers_table.c.SingerId >= cursor).order_by(singers_table.c.SingerId.asc()).limit(page_size)
-        result = session.execute(stmt)
-        for row in result:
-            data["records"].append(row._asdict())
-            data["next_cursor"] = int(row.SingerId)+1
-
-    return data
-
-###############################################################################
-# 3. Basic API with SQL Alchemy ORM & cursor based pagination
+# Basic API with SQL Alchemy ORM & cursor based pagination
 # ---------------------
 # Uses the Google created spanner dialect for SQLAlchemy
 # https://github.com/googleapis/python-spanner-sqlalchemy
 ###############################################################################
 @musicapi.get("/api/v1/singers")
-def fetch_all_singers_paginated_sqlalchemy(cursor: int, page_size: int):
-
-    # fetch environment variables to tell SQLAlchemy where to find our DB
-    instanceid = os.environ["SPANNER_INSTANCE_ID"]
-    databaseid = os.environ["SPANNER_DATABASE_ID"]
-    projectid = os.environ["GOOGLE_CLOUD_PROJECT"]
-
-    # The start of any SQLAlchemy application is an object called the Engine. 
-    # This object acts as a central source of connections to a particular database
-    # echo=True instructs the Engine to log all of the SQL it emits to a Python logger that will write to sout
-    engine = create_engine(
-        f"spanner+spanner:///projects/{projectid}/instances/{instanceid}/databases/{databaseid}",
-        echo=True
-    )
+def fetch_all_singers(cursor: int, page_size: int, engine = Depends(get_db_engine), metadata = Depends(get_db_metadata)):
     
-    # Database metadata refers to objects that represnt Tables & Columns
-    # MetaData is a collection (e.g. py dict) that stores Tables
-    # table = Table("Singers", MetaData(bind=engine), autoload=True)
-    metadata_obj = MetaData()
-
-    # Once we have a MetaData object, we can declare some Table objects
-    # We use a feature of SQLAlchemy called table reflection to reduce code required
-    # Table reflection refers to the process of generating Table 
-    # and related objects by reading the current state of a database. 
-    # This way we avoid hardcoding Table schema which can go out of date !
-    singers_table = Table("Singers", metadata_obj, autoload_with=engine)
-    
-    # create a container to store the information fetched from the db
-    # set default value of "next_cursor" to current cursor to handle
-    # edge case when we have reached the end of the data in the db
-    data = {"records":[], "next_cursor": cursor}
-
-    # Using a session, first create a select statement
-    # implement cursor based pagination used the provided cursor & page_size provided in the api call
-    # parse the result set into a json object which can be returned
-    with Session(engine) as session:
-        stmt = select(singers_table).where(singers_table.c.SingerId >= cursor).order_by(singers_table.c.SingerId.asc()).limit(page_size)
-        result = session.execute(stmt)
-        for row in result:
-            data["records"].append(row._asdict())
-            data["next_cursor"] = int(row.SingerId)+1
-
+    tbl_name: str = "Singers"
+    tbl, pk_col, pk_col_name = get_info_tbl_with_1_col_pk(tbl_name, metadata)
+    data: dict = get_page_of_data(engine, tbl, pk_col, pk_col_name, cursor, page_size)
     return data
 
-###############################################################################
-# 3. Basic API with SQL Alchemy ORM & cursor based pagination
-# ---------------------
-# Uses the Google created spanner dialect for SQLAlchemy
-# https://github.com/googleapis/python-spanner-sqlalchemy
-###############################################################################
 @musicapi.get("/api/v1/albums")
-def fetch_all_albums_paginated_sqlalchemy(cursor: int, page_size: int):
+def fetch_all_albums(cursor: int, page_size: int, engine = Depends(get_db_engine), metadata = Depends(get_db_metadata)):
 
-    # fetch environment variables to tell SQLAlchemy where to find our DB
-    instanceid = os.environ["SPANNER_INSTANCE_ID"]
-    databaseid = os.environ["SPANNER_DATABASE_ID"]
-    projectid = os.environ["GOOGLE_CLOUD_PROJECT"]
-
-    # The start of any SQLAlchemy application is an object called the Engine. 
-    # This object acts as a central source of connections to a particular database
-    # echo=True instructs the Engine to log all of the SQL it emits to a Python logger that will write to sout
-    engine = create_engine(
-        f"spanner+spanner:///projects/{projectid}/instances/{instanceid}/databases/{databaseid}",
-        echo=True
-    )
-    
-    # Database metadata refers to objects that represnt Tables & Columns
-    # MetaData is a collection (e.g. py dict) that stores Tables
-    # table = Table("Singers", MetaData(bind=engine), autoload=True)
-    metadata_obj = MetaData()
-
-    # Once we have a MetaData object, we can declare some Table objects
-    # We use a feature of SQLAlchemy called table reflection to reduce code required
-    # Table reflection refers to the process of generating Table 
-    # and related objects by reading the current state of a database. 
-    # This way we avoid hardcoding Table schema which can go out of date !
-    my_table = Table("Albums", metadata_obj, autoload_with=engine)
-    my_table_pk = my_table.c.AlumbId
-    
-    # create a container to store the information fetched from the db
-    # set default value of "next_cursor" to current cursor to handle
-    # edge case when we have reached the end of the data in the db
-    data = {"records":[], "next_cursor": cursor}
-
-    # Using a session, first create a select statement
-    # implement cursor based pagination used the provided cursor & page_size provided in the api call
-    # parse the result set into a json object which can be returned
-    with Session(engine) as session:
-        stmt = select(my_table).where(my_table_pk >= cursor).order_by(my_table_pk.asc()).limit(page_size)
-        result = session.execute(stmt)
-        for row in result:
-            data["records"].append(row._asdict())
-            data["next_cursor"] = int(row.AlumbId)+1
-
+    tbl_name: str = "Albums"
+    tbl, pk_col, pk_col_name = get_info_tbl_with_1_col_pk(tbl_name, metadata)
+    data: dict = get_page_of_data(engine, tbl, pk_col, pk_col_name, cursor, page_size)
     return data
 
-###############################################################################
-# 3. Basic API with SQL Alchemy ORM & cursor based pagination
-# ---------------------
-# Uses the Google created spanner dialect for SQLAlchemy
-# https://github.com/googleapis/python-spanner-sqlalchemy
-###############################################################################
 @musicapi.get("/api/v1/labels")
-def fetch_all_labels_paginated_sqlalchemy(cursor: int, page_size: int):
-
-    # fetch environment variables to tell SQLAlchemy where to find our DB
-    instanceid = os.environ["SPANNER_INSTANCE_ID"]
-    databaseid = os.environ["SPANNER_DATABASE_ID"]
-    projectid = os.environ["GOOGLE_CLOUD_PROJECT"]
-
-    # The start of any SQLAlchemy application is an object called the Engine. 
-    # This object acts as a central source of connections to a particular database
-    # echo=True instructs the Engine to log all of the SQL it emits to a Python logger that will write to sout
-    engine = create_engine(
-        f"spanner+spanner:///projects/{projectid}/instances/{instanceid}/databases/{databaseid}",
-        echo=True
-    )
-    
-    # Database metadata refers to objects that represnt Tables & Columns
-    # MetaData is a collection (e.g. py dict) that stores Tables
-    # table = Table("Singers", MetaData(bind=engine), autoload=True)
-    metadata_obj = MetaData()
-
-    # Once we have a MetaData object, we can declare some Table objects
-    # We use a feature of SQLAlchemy called table reflection to reduce code required
-    # Table reflection refers to the process of generating Table 
-    # and related objects by reading the current state of a database. 
-    # This way we avoid hardcoding Table schema which can go out of date !
-    my_table = Table("Labels", metadata_obj, autoload_with=engine)
-    my_table_pk = my_table.c.LabelId
-    
-    # create a container to store the information fetched from the db
-    # set default value of "next_cursor" to current cursor to handle
-    # edge case when we have reached the end of the data in the db
-    data = {"records":[], "next_cursor": cursor}
-
-    # Using a session, first create a select statement
-    # implement cursor based pagination used the provided cursor & page_size provided in the api call
-    # parse the result set into a json object which can be returned
-    with Session(engine) as session:
-        stmt = select(my_table).where(my_table_pk >= cursor).order_by(my_table_pk.asc()).limit(page_size)
-        result = session.execute(stmt)
-        for row in result:
-            data["records"].append(row._asdict())
-            data["next_cursor"] = int(row.LabelId)+1
-
+def fetch_all_labels(cursor: int, page_size: int, engine = Depends(get_db_engine), metadata = Depends(get_db_metadata)):
+    tbl_name: str = "Labels"
+    tbl, pk_col, pk_col_name = get_info_tbl_with_1_col_pk(tbl_name, metadata)
+    data: dict = get_page_of_data(engine, tbl, pk_col, pk_col_name, cursor, page_size)
     return data
